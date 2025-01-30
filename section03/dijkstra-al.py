479c3b1e9b6ede104f5ff0343c54dc35b5fcd477
XZL6CP <alex.peplinski@gm.com>
2025-01-30 16:03:18 -0500

    adding edge and vertex code

--- section03/dijkstra-al.py
+++ section03/dijkstra-al.py
@@ -17,16 +17,30 @@ class Graph(object):
         """
         print(self._verticies_formal_description)
         print(self._edges_formal_description)
+
+    def scan_for_vertex(self, desired_vertex):
+        """
+        scans for vertex using a String
+        """
+        for vertex_string, vertex_object in self._verticies_formal_description.items():
+            print(f"checking vertex: {vertex_string}")
+            #vertex_object.print_name()
+            if vertex_string == desired_vertex:
+                vertex_object.print_name()
+                return vertex_object
+
     
-    def scan_for_specific_vertex(self, vertexName):
+    def scan_for_edge(self, starting_vertex, ending_vertex):
         """
-        scans for vertex using adjacent and neighbors
+        scans for edge using adjacent and neighbors
         """
-        for edge in self._edges_formal_description:
-            #edge.print_edge_points()
-            for vertex in edge.get_edge_points():
-                if str(vertex) == str(vertexName):
-                    print(edge.get_edge_points())
+        for edge_string, edge_object in self._edges_formal_description.items():
+            #print(f"checking edge: {edge_string}")
+            #edge_object.print_edge_points()
+            desired_edge = starting_vertex + " , " + ending_vertex
+            if edge_string == desired_edge:
+                edge_object.print_edge_points()
+                return edge_object
     
     def set_vertex(self, vertex_object):
         """
@@ -40,12 +54,15 @@ class Graph(object):
         """
         return self._verticies_formal_description[vertex_name]
 
-    def set_edge(self, edge_object):
+    def set_edge(self, starting_vertex, ending_vertex, edge_object):
         """
         sets a edge...
         """
+        edge_string = starting_vertex + " , " + ending_vertex
+        print(f"Edge String: {edge_string}")
         print(type(edge_object.get_edge_points()))
-        self._edges_formal_description[edge_object] = edge_object
+        
+        self._edges_formal_description[edge_string] = edge_object
 
     def get_edge(self, edge_name):
         """
@@ -55,9 +72,7 @@ class Graph(object):
 
 
 
-
-
-class Vertex[T](Graph):
+class Vertex(Graph):
     """
     Provides operations to interact with verticies
     """
@@ -67,6 +82,7 @@ class Vertex[T](Graph):
         Initializes a new instance of the vertex class
         """
         self._vertex_name = vertexName
+        self._vertext_distance = 999999999
         self._vertex: dict[int,int] = {}
     
     def get_name(self):
@@ -80,6 +96,18 @@ class Vertex[T](Graph):
         Effects: prints the vertext name
         """
         print(self._vertex_name)
+    
+    def set_distance(self, distance):
+        """
+        Effects: sets the vertext distance
+        """
+        self._vertext_distance = distance
+
+    def get_distance(self):
+        """
+        Effects: returns the vertext distance
+        """
+        return self._vertext_distance
 
 class Edge(Graph):
     """
@@ -90,8 +118,15 @@ class Edge(Graph):
         """
         Initializes a new instance of the edge class
         """
-        self._edge_points = { starting_vertex , ending_vertex }
+        self._edge_points = { "starting_vertex": starting_vertex , "ending_vertex": ending_vertex }
+        self._edge_weight = 0
         self._edge: dict[int,int] = {}
+
+    def set_edge_weight(self, weight):
+        """
+        Effects: returns the edge verticies
+        """
+        self._edge_weight = weight
     
     def get_edge_points(self):
         """
@@ -104,6 +139,17 @@ class Edge(Graph):
         Effects: prints the edge points
         """
         print(self._edge_points)
+    def get_edge_weight(self):
+        """
+        Effects: returns the edge verticies
+        """
+        return self._edge_weight
+
+    def print_edge_weight(self):
+        """
+        Effects: prints the edge points
+        """
+        print(self._edge_weight)
 
 
 
@@ -122,19 +168,27 @@ def main():
         if has_connection == 'y':
             connected_vertex = input("Enter a connected Vertex: ")
             edge = Edge(vertex.get_name(), connected_vertex)
+            edge_weight = input("Enter an edge weight: ")
+            edge.set_edge_weight(edge_weight)
             edge.print_edge_points()
-            graph_g1.set_edge(edge)
+            edge.print_edge_weight()
+            graph_g1.set_edge(vertex.get_name(), connected_vertex, edge)
             graph_g1.print_graph()
 
         loop_again = input("Enter another vertex?(y/n): ")
         if loop_again == 'n':
             break
 
-    vertex_check = input("which vertex do you want to check?: ")
-    selected_vertex = graph_g1.get_vertex(vertex_check)
-    selected_vertex.print_name()
+    start_vertex_check = input("which vertex do you want to start at?: ")
+    selected_starting_vertex = graph_g1.get_vertex(start_vertex_check)
 
-    graph_g1.scan_for_specific_vertex(selected_vertex.get_name())
+    end_vertex_check = input("which vertex do you want to end at?: ")
+    selected_ending_vertex = graph_g1.get_vertex(end_vertex_check)
+
+    selected_edge = graph_g1.scan_for_edge(start_vertex_check, end_vertex_check)
+    selected_edge.print_edge_weight()
+
+    selected_vertex = graph_g1.scan_for_vertex(start_vertex_check)
 
 
 if __name__ == "__main__":
