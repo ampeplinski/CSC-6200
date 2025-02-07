58946c87dde65001d87d1bb055b1ab479c6193ef
ampeplinski <alex.peplinski@student.cuaa.edu>
2025-02-07 10:11:37 -0500

    adding loops

--- section05/main01.cpp
+++ section05/main01.cpp
@@ -15,24 +15,37 @@ int main(){
     string s2;
     cout << "type a string" << endl;
     cin >> s2;
-    vector< vector<int> > table(s1.length()+1, vector<int>(s2.length()+ 1, 0));
+    vector< vector<int> > table(s1.length() + 1, vector<int>(s2.length()+ 1, 0));
+    vector< vector<int> > printTable(s1.length() + 2, vector<int>(s2.length()+ 2, 0));
+    
     //string *ptr = dp;
     int maxLength = 0;
     int endIndex = 0;
 
     cout << s1 << endl;
-    for (int i = 0; i < dp.size(); i++){
-        //cout << "type a string"<< dp[i] << endl;
-        for ( int j = 0; j < dp[i].size(); j++){
+    for (int i = 0; i < table.size(); i++){
+        if (i == 0){
+            continue;
+        }
+        cout << "string1: "<< s1[i] << endl;
+        
+        for ( int j = 0; j < table[i].size(); j++){
+            if (j == 0) {
+                continue;
+            }
+            //cout << "table: "<< table[i][j] << endl;
+            //cout << "string2: "<< s2[j] << endl;
+            //cout << "string2: "<< s2[j-1] << endl;
             if (s1[i-1]== s2[j-1]){
+
                 table[i][j] = table[i-1][j-1] +1;
                 if(table[i][j] > maxLength) {
                     maxLength = table[i][j];
                     endIndex = i;
                 }
             }
-            if (j == 0){
-                cout << s2[i] << endl;
+            else{
+                table[i][j] = 0;
             }
             //cout << "type a string"<< dp[i][j] << endl;
             }
