98d8ccc373ff0f4d447ede67966ca6c548062ed5
ampeplinski <alex.peplinski@student.cuaa.edu>
2025-02-07 09:23:36 -0500

    section05 init commit

---
+++ section05/main01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+//#include <map>
+#include <string>
+//#include <list>
+//#include <set>
+//#include <iterator>
+using namespace std;
+
+int main(){
+    string s1;
+    cout << "type a string" << endl;
+    cin >> s1;
+
+    string s2;
+    cout << "type a string" << endl;
+    cin >> s2;
+}
