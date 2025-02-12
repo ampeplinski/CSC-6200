2e21d958b1c202342f663855ddf5bb01f253d895
ampeplinski <alex.peplinski@student.cuaa.edu>
2025-02-12 13:29:15 -0500

    create min heap

---
+++ section06/main01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//#include <vector>
+//#include <map>
+//#include <string>
+//#include <list>
+//#include <set>
+//#include <iterator>
+using namespace std;
+
+//       5
+//     /  \
+//    9    12
+//   / \   /  
+// 13  16 45     
+
+int minHeap[6] = {5, 9, 12, 13, 16, 45};
