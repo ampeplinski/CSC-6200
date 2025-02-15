e6de8a5c60b691377b1f329950a035bec5ea46ad
ampeplinski <alex.peplinski@student.cuaa.edu>
2025-02-15 16:29:55 -0500

    heapify

--- section06/main01.cpp
+++ section06/main01.cpp
@@ -42,9 +42,9 @@ class bSearchTree : public binaryTree
 {
     //binaryTreeNode *root;
     public:
-        bool search(const int& searchItem) const{
-        binaryTreeNode* current;
-        bool found = false;
+        //bool search(const int& searchItem) const{
+        //binaryTreeNode* current;
+        //bool found = false;
 
 
         //if (root == NULL){
@@ -57,22 +57,23 @@ class bSearchTree : public binaryTree
         //            set found to true;
         //        else    
         //}
-        }
-        void bSearchTree::insert(const int& insertItem){
+        //}
+        //void bSearchTree::insert(const int& insertItem);//{
             // stuff
-            binaryTreeNode* current;
-            binaryTreeNode* tailCurrent;
-            binaryTreeNode* newNode;
-            newNode = new binaryTreeNode;
+            //binaryTreeNode* current;
+            //binaryTreeNode* tailCurrent;
+            //binaryTreeNode* newNode;
+            //newNode = new binaryTreeNode;
             //newNode->info = insertItem;
-            newNode->llink = NULL;
-            newNode->rlink = NULL;
+            //newNode->llink = NULL;
+            //newNode->rlink = NULL;
             //if(root == NULL)
             //    root = newNode;
             //else{
             //    current = root;
             //}
-            };
+            //return
+            //};
         void deleteNode(const int& deleteItem);
             // other things
     private:
@@ -94,7 +95,7 @@ class Node {
         Node* current;
         Node* tailCurrent;
         Node* newNode;
-        Node *newNode = new Node();
+        //Node *newNode = new Node();
         //nextNode->letter_number = letter_number;
         //nextNode->Left = NULL;
         //nextNode->Right = NULL;
@@ -133,12 +134,13 @@ class Node {
     
 };
 int parent (int i) {
+    cout << i << endl;
     return (i -1)/2;
 }
 void heapify(vector<simpleTreeNode> minHeap){
     int i = minHeap.size() - 1;
     // Step 3 Compare the value of this child node with its parent
-    while( i  > 0 ){
+    while( i  > 0 && minHeap[parent(i)].frequency > minHeap[i].frequency ){
         // Step 4 If value of parent is more than child, then swap them.
         if (minHeap[parent(i)].frequency > minHeap[i].frequency ){
             swap(minHeap[i], minHeap[parent(i)]);
@@ -166,7 +168,7 @@ int main(){
     vector<simpleTreeNode> minHeap;
     minHeap.push_back(simpleTreeNode("a",5));
     // heapify up
-    heapify(minHeap);
+    //heapify(minHeap);
     minHeap.push_back(simpleTreeNode("b",9));
     heapify(minHeap);
     minHeap.push_back(simpleTreeNode("c",12));
@@ -178,51 +180,51 @@ int main(){
     minHeap.push_back(simpleTreeNode("f",45));
     heapify(minHeap);
 
-    for (int n = 0; n < 6; n++){
+    for (int n = 0; n < minHeap.size(); n++){
         cout << minHeap[n].value << minHeap[n].frequency << endl;
     }
 
     //string minHeapArray[6] = {"a/5", "b/9", "c/12", "d/13", "e/16", "f/45"};
-    for (int n = 0; n < 6; n++){
-        int slash = linkedlist[n].find('/');
-        int selected = stoi(linkedlist[n].substr(slash + 1));
-        cout << "selected: " << linkedlist[n] << endl;
-
-        if (selected < stoi(smallest_left.substr(slash + 1))){
-            smallest_left = linkedlist[n];
-            continue;
-        };
-        if (selected < stoi(smallest_right.substr(slash + 1))){
-                smallest_right = linkedlist[n];
-            }
-    }
-    cout << "Two min frequ nodes: " << smallest_left << ", " << smallest_right << endl;
-    //auto itr = find(linkedlist.begin(), linkedlist.end(), smallest_left);
-    linkedlist.erase(find(linkedlist.begin(), linkedlist.end(), smallest_left));
-    linkedlist.erase(find(linkedlist.begin(), linkedlist.end(), smallest_right));
+    //for (int n = 0; n < 6; n++){
+    //    int slash = linkedlist[n].find('/');
+    //    int selected = stoi(linkedlist[n].substr(slash + 1));
+    //    cout << "selected: " << linkedlist[n] << endl;
+    //
+    //     if (selected < stoi(smallest_left.substr(slash + 1))){
+    //         smallest_left = linkedlist[n];
+    //         continue;
+    //     };
+    //     if (selected < stoi(smallest_right.substr(slash + 1))){
+    //             smallest_right = linkedlist[n];
+    //         }
+    // }
+    // cout << "Two min frequ nodes: " << smallest_left << ", " << smallest_right << endl;
+    // //auto itr = find(linkedlist.begin(), linkedlist.end(), smallest_left);
+    // linkedlist.erase(find(linkedlist.begin(), linkedlist.end(), smallest_left));
+    // linkedlist.erase(find(linkedlist.begin(), linkedlist.end(), smallest_right));
     
-    int slash = smallest_left.find('/');
-    new_node = stoi(smallest_left.substr(slash + 1)) + stoi(smallest_right.substr(slash + 1));
-    cout << "new_node: " << new_node << endl;
-    Node current = Node(); // make it not set
-    //current.push_left(smallest_left);
-    //current.push_left(smallest_right);
+    // int slash = smallest_left.find('/');
+    // new_node = stoi(smallest_left.substr(slash + 1)) + stoi(smallest_right.substr(slash + 1));
+    // cout << "new_node: " << new_node << endl;
+    // Node current = Node(); // make it not set
+    // //current.push_left(smallest_left);
+    // //current.push_left(smallest_right);
 
     
 
-    for (int i = 0; i < 6; i++){
-            if (stoi(linkedlist[i+1].substr(slash + 1)) > new_node){
-                if (stoi(linkedlist[i].substr(slash + 1)) < new_node) {
-                    cout << "larger: " << linkedlist[i+1] << " smaller: " << linkedlist[i] << endl;
-                    cout << "i: " << i << endl;
-                    linkedlist.insert( linkedlist.begin() + i, to_string(new_node));
-                    break;
-                }
-            }
-        }
+    // for (int i = 0; i < 6; i++){
+    //         if (stoi(linkedlist[i+1].substr(slash + 1)) > new_node){
+    //             if (stoi(linkedlist[i].substr(slash + 1)) < new_node) {
+    //                 cout << "larger: " << linkedlist[i+1] << " smaller: " << linkedlist[i] << endl;
+    //                 cout << "i: " << i << endl;
+    //                 linkedlist.insert( linkedlist.begin() + i, to_string(new_node));
+    //                 break;
+    //             }
+    //         }
+    //     }
 
-    for (int z = 0; z < 6; z++){
-        cout << linkedlist[z] << endl;
-    }
+    // for (int z = 0; z < 6; z++){
+    //     cout << linkedlist[z] << endl;
+    // }
 
 }
