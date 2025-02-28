4d12c718cd0b227604ae2e95ce7e6ddb3bce6663
ampeplinski <alex.peplinski@student.cuaa.edu>
2025-02-28 12:26:16 -0500

    decimal to binary 64

--- section07/final01.cpp
+++ section07/final01.cpp
@@ -24,29 +24,64 @@ vector<int> strToBinary(string inputValue){
 
 }
 
+vector<int> decToBinary(int num){
+    int binaryNum[16];
+    vector<int> reversedBin;
+
+    int i = 0;
+    while (num > 0){
+        binaryNum[i] = num % 2;
+        num = num / 2;
+        i++;
+    };
+
+    cout << sizeof(binaryNum)<< endl;
+    //for (int i = 0;  i < sizeof(binaryNum); ++i){
+    //    cout << binaryNum[i];
+    //};
+    for (int n = 0;  n < i; ++n){
+        //cout << binaryNum[n];
+        reversedBin.insert(reversedBin.begin(), binaryNum[n]);
+    };
+
+    while (reversedBin.size() < 64){
+        reversedBin.insert(reversedBin.begin(), 0);
+    }
+    //for ( int j = i -1; j >= 0; j--){
+    //    cout<< binaryNum[j];
+    //    reversedBin[]
+    //}
+    for (int i = 0;  i < reversedBin.size(); ++i){
+        cout << reversedBin[i];
+    };
+    cout << endl;
+    return reversedBin;
+}
+
 vector<int>  preprocess(vector<int> binList){
-    int messageLength = binList.size() * 8;
+    int messageLength = binList.size();
     // append the bit "1" followed by k zero bits
     //string lastLetter = "10000000";
     binList.push_back(1);
     //where k is the smalles non-negative solution to the equation
-    int k = 448;
-    // l + 1 + k = 448mod512
-    int checker = messageLength + 1 + k ;
+    int k = 351;
+    // l + 1 + k = 448 mod512
+    int checker = messageLength + 1 + k + 64;
     cout << "checker: " << checker << endl;
-    int remainder = checker % 512;
-    cout << "remainder: " << checker << endl;
-    cout << binList.size() * 8 << endl;
+    //int remainder = checker % 512;
+    //cout << "remainder: " << checker << endl;
+    //cout << binList.size() << endl;
 
-    for (int i = 0;  i < 43; ++i){
-        binList.push_back("00000000");
-    };
+    //for (int i = 0;  i < 43; ++i){
+    //    binList.push_back("00000000");
+    //};
     // 64 bit block equal to the number l expressed using binary representation
-    for (int i = 0;  i < 7; ++i){
-        binList.push_back("00000000");
-    };
-    string binMsgLength = "01100000";
-    binList.push_back(binMsgLength);
+    //for (int i = 0;  i < 7; ++i){
+    //    binList.push_back("00000000");
+    //};
+    //string binMsgLength = "01100000";
+    //binList.push_back(binMsgLength);
+    vector<int> lengthbits = decToBinary(messageLength);
 
     return binList;
     
@@ -55,11 +90,11 @@ vector<int>  preprocess(vector<int> binList){
 int main(){
     string inputvalue = "Hello Worlds";
     vector<int> binList = strToBinary(inputvalue);
-    int messageLength = binList.size() * 8;
+    int messageLength = binList.size();
     cout << messageLength << endl;
-    vector<string> proccessedBinList = preprocess(binList);
+    vector<int> proccessedBinList = preprocess(binList);
 
-    int msgAndBufferLength = proccessedBinList.size() * 8;
+    int msgAndBufferLength = proccessedBinList.size();
     cout << msgAndBufferLength << endl;
     //for (int i = 0;  i < proccessedBinList.size(); i++){
      //   cout << proccessedBinList[i] << endl;
